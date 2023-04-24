49525aafc2569cb28e031091b80f613f914dc2dc
Sven <75948709+codingholt@users.noreply.github.com>
2023-04-24 11:22:11 +0200

    move pages to electron

--- renderer/pages/home.tsx
+++ renderer/pages/home.tsx
@@ -1,30 +1,127 @@
-import React from 'react';
-import Head from 'next/head';
-import Link from 'next/link';
+import { NextPage } from "next";
+import { useRouter } from "next/router";
+import { FormEventHandler, useState } from "react";
 
-function Home() {
-  return (
-    <React.Fragment>
-      <Head>
-        <title>Home - Nextron (with-typescript-tailwindcss)</title>
-      </Head>
-      <div className='grid grid-col-1 text-2xl w-full text-center'>
-        <img className='ml-auto mr-auto' src='/images/logo.png' />
-        <span>⚡ Electron ⚡</span>
-        <span>+</span>
-        <span>Next.js</span>
-        <span>+</span>
-        <span>tailwindcss</span>
-        <span>=</span>
-        <span>💕 </span>
-      </div>
-      <div className='mt-1 w-full flex-wrap flex justify-center'>
-        <Link href='/next'>
-          <a className='btn-blue'>Go to next page</a>
-        </Link>
-      </div>
-    </React.Fragment>
-  );
-}
+const Home: NextPage = () => {
+	const [mailServerData, setMailServerData] = useState({
+		email: "",
+		username: "",
+		password: "",
+		accountType: "IMAP",
+		incomingServer: "",
+		outgoingServer: "",
+	});
+
+	const handleSubmit: FormEventHandler<HTMLFormElement> = async (e) => {
+		console.log("submitting");
+	};
+
+	const router = useRouter();
+	return (
+		<div>
+			<form className="w-40" onSubmit={handleSubmit}>
+				<div className="flex flex-col">
+					<label htmlFor="email">E-mailadress</label>
+					<input
+						type="email"
+						id="email"
+						className="border"
+						onChange={({ target }) =>
+							setMailServerData({
+								...mailServerData,
+								email: target.value,
+							})
+						}
+					/>
+				</div>
+
+				<div className="flex flex-col">
+					<label htmlFor="username">Username</label>
+					<input
+						type="text"
+						id="username"
+						className="border"
+						onChange={({ target }) =>
+							setMailServerData({
+								...mailServerData,
+								username: target.value,
+							})
+						}
+					/>
+				</div>
+
+				<div className="flex flex-col">
+					<label htmlFor="password">Password</label>
+					<input
+						type="password"
+						id="password"
+						className="border"
+						onChange={({ target }) =>
+							setMailServerData({
+								...mailServerData,
+								password: target.value,
+							})
+						}
+					/>
+				</div>
+
+				<div className="flex flex-col">
+					<label htmlFor="account-type">Account type</label>
+					<select
+						disabled
+						id="account-type"
+						onChange={({ target }) =>
+							setMailServerData({
+								...mailServerData,
+								accountType: target.value,
+							})
+						}
+					>
+						<option value="IMAP">IMAP</option>
+						<option value="POP">POP</option>
+					</select>
+				</div>
+
+				<div className="flex flex-col">
+					<label htmlFor="incoming-server">
+						Incoming Mail Server
+					</label>
+					<input
+						type="text"
+						id="incoming-server"
+						className="border"
+						onChange={({ target }) =>
+							setMailServerData({
+								...mailServerData,
+								incomingServer: target.value,
+							})
+						}
+					/>
+				</div>
+
+				<div className="flex flex-col">
+					<label htmlFor="outgoing-server">
+						Outgoing Mail Server
+					</label>
+					<input
+						type="text"
+						id="outgoing-server"
+						className="border"
+						onChange={({ target }) =>
+							setMailServerData({
+								...mailServerData,
+								outgoingServer: target.value,
+							})
+						}
+					/>
+				</div>
+
+				<button type="submit" className="mt-6 w-full border p-2">
+					Submit
+				</button>
+			</form>
+		</div>
+	);
+};
 
 export default Home;
